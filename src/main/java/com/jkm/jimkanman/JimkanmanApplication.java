ad14e7dc6dc82502726b800a601da1ec951874b8
Jaehwan Hyun <jec0101@naver.com>
2024-10-05 14:14:17 +0900

    Initial Commit

---
+++ src/main/java/com/jkm/jimkanman/JimkanmanApplication.java
@@ -0,0 +1,13 @@
+package com.jkm.jimkanman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimkanmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimkanmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jkm/jimkanman/JimkanmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkm.jimkanman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimkanmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
