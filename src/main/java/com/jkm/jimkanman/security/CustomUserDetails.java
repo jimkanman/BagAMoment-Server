1beaf4704a77f70b50bf3b994fa2db46c354cc13
Jaehwan Hyun <jec0101@naver.com>
2024-11-01 16:30:54 +0900

    add: UserDetails와 UserDetailsService 구현

---
+++ src/main/java/com/jkm/jimkanman/security/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.jkm.jimkanman.security;
+
+import com.jkm.jimkanman.domain.Member;
+import com.jkm.jimkanman.domain.enums.MemberStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final Member member;
+
+
+    public Member getMember(){return member;}
+
+    public String getLoginId(){return member.getLoginId();}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return member.getRoles()
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !member.getStatus().equals(MemberStatus.SUSPENDED);
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !member.getStatus().equals(MemberStatus.INACTIVE); // 비밀번호 만료 여부
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return member.getStatus().equals(MemberStatus.ACTIVE);
+    }
+}
---
+++ src/main/java/com/jkm/jimkanman/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.jkm.jimkanman.security;
+
+import com.jkm.jimkanman.repository.MemberRepository;
+import com.jkm.jimkanman.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new CustomUserDetails(memberRepository.findByLoginId(username)
+                .orElseThrow(() -> new NoSuchElementException("해당 회원 정보를 찾을 수 없습니다")));
+    }
+}
