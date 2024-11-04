60e961a8cfbd8b4f981776c0e28691600c433d02
Jaehwan Hyun <jec0101@naver.com>
2024-11-04 21:52:14 +0900

    update: Exception 핸들링 추가

--- src/main/java/com/jkm/jimkanman/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jkm/jimkanman/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jkm.jimkanman.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +15,22 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Object> handleRuntimeException(RuntimeException e, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", e.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", e.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(MethodArgumentNotValidException e, WebRequest request) {
         Map<String, Object> body = new HashMap<>();
         body.put("message", e.getMessage());
 
