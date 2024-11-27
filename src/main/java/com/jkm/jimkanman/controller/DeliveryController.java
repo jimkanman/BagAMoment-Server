f96737a5aa598eee31ec515ee876f0cba51fd83b
Jaehwan Hyun <jec0101@naver.com>
2024-11-28 02:04:47 +0900

    add: 컨트롤러 url 매핑

--- src/main/java/com/jkm/jimkanman/controller/DeliveryController.java
+++ src/main/java/com/jkm/jimkanman/controller/DeliveryController.java
@@ -1,8 +1,6 @@
 package com.jkm.jimkanman.controller;
 
-import com.jkm.jimkanman.dto.DeliveryRequest;
-import com.jkm.jimkanman.dto.ReservationRequest;
-import com.jkm.jimkanman.dto.ReservationResponse;
+import com.jkm.jimkanman.dto.*;
 import com.jkm.jimkanman.global.success.SuccessResponse;
 import com.jkm.jimkanman.service.DeliveryService;
 import com.jkm.jimkanman.service.StorageService;
@@ -10,10 +8,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Tag(name = "배송 API", description = "배송 관련 API입니다")
@@ -35,55 +32,58 @@ public class DeliveryController {
      * */
 
     /** 배송 신청하기 */
-    public ResponseEntity<SuccessResponse<?>> registerDelivery(
+    @PostMapping("/storages/{storageId}/delivery-reservations")
+    public ResponseEntity<SuccessResponse<ReservationResponse.ReservationDto>> registerDelivery(
             @PathVariable("storageId")Long storageId,
             @Valid @RequestBody DeliveryRequest.ReservationDto reservationDto
     ) {
-        // 예약 & 배송 예약 생성
-        // 배송 객체도 만들어둔 후 status PENDING으로 남김
-
         // 예약 생성
         ReservationResponse.ReservationResultDto reservationResult
                 = storageService.makeReservation(storageId, new ReservationRequest.ReservationDto(reservationDto));
 
         // 배송예약 & 배송 객체 생성
-        deliveryService.makeDeliveryReservation(reservationResult.getId(), reservationDto);
-        return null;
+        ReservationResponse.ReservationDto deliveryReservationResult = deliveryService.makeDeliveryReservation(reservationResult.getId(), reservationDto);
+        return SuccessResponse.ok(deliveryReservationResult);
     }
 
     /** 배송기사 배정하기 */
-    public ResponseEntity<SuccessResponse<?>> assignDelivery(@PathVariable("deliveryId") Long deliveryId) {
+    @PostMapping("/delivery/assign")
+    public ResponseEntity<SuccessResponse<DeliveryResponse.SimpleDeliveryDto>> assignDelivery(@PathVariable("deliveryId") Long deliveryId) {
         //배송 객체 status 업데이트 -> ASSIGNED
-        deliveryService.assignDelivery(deliveryId);
-        // id 반환
-        return null;
+        DeliveryResponse.SimpleDeliveryDto simpleDeliveryDto = deliveryService.assignDelivery(deliveryId);
+        return SuccessResponse.ok(simpleDeliveryDto); // 배송 id 반환
     }
 
     /** PENDING 상태인 최근 배송 조회  */
-    public ResponseEntity<SuccessResponse<?>> getRecentDeliveries() {
+    @GetMapping("/delivery")
+    public ResponseEntity<SuccessResponse<List<DeliveryResponse.DeliveryDto>>> getRecentDeliveries() {
         // 배송 예약 객체 created_at 기준 정렬 후 조회
-        deliveryService.getPendingDeliveries();
-        return null;
+        List<DeliveryResponse.DeliveryDto> pendingDeliveries = deliveryService.getPendingDeliveries();
+        return SuccessResponse.ok(pendingDeliveries);
     }
 
     /** 배송 시작하기 */
-    public ResponseEntity<SuccessResponse<?>> startDelivery(
+    @PostMapping("/delivery/{deliveryId}/location")
+    public ResponseEntity<SuccessResponse<Object>> startDelivery(
             @PathVariable("deliveryId") Long deliveryId,
             @RequestParam Double latitude,
             @RequestParam Double longitude
     ) {
         // 배송 객체 status 업데이트 + 요청받은 위치로 배송 객체 초기화
         deliveryService.startDelivery(deliveryId, latitude, longitude);
-        return null;
+        return SuccessResponse.ok(null);
     }
 
+    @PutMapping("/delivery/{deliveryId}/location")
     public ResponseEntity<SuccessResponse<?>> updateDeliveryLocation() {
         // polling 방식
         
         return null;
     }
 
-    public ResponseEntity<SuccessResponse<?>> getDeliveryLocation() {
+    @GetMapping("/delivery/{deliveryId}/location")
+    public ResponseEntity<SuccessResponse<LocationDto>> getDeliveryLocation(@PathVariable("deliveryId") Long deliveryId) {
+        LocationDto location = deliveryService.getDeliveryLocation(deliveryId);
         return null;
     }
 
--- src/main/java/com/jkm/jimkanman/dto/DeliveryResponse.java
+++ src/main/java/com/jkm/jimkanman/dto/DeliveryResponse.java
@@ -1,9 +1,79 @@
 package com.jkm.jimkanman.dto;
 
+import com.jkm.jimkanman.converter.StringToDateTimeConverter;
+import com.jkm.jimkanman.domain.Delivery;
+import com.jkm.jimkanman.domain.DeliveryReservation;
+import com.jkm.jimkanman.domain.enums.DeliveryStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class DeliveryResponse {
     private DeliveryResponse() {}
-    
+
+    /** 배송 예약에 대한 Dto */
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
     public static class ReservationDto {
-        // 배송 객체 포함?
+        private Long id;
+        private Long deliveryId;
+        private String deliveryArrivalDateTime;
+        private String destinationAddress;
+        private String destinationPostalCode;
+        private DeliveryStatus status;
+
+        public ReservationDto(DeliveryReservation deliveryReservation) {
+            id = deliveryReservation.getId();
+            deliveryId = deliveryReservation.getDelivery().getId();
+            deliveryArrivalDateTime = StringToDateTimeConverter.toDateString(deliveryReservation.getDeliveryArrivalDateTime());
+            destinationAddress = deliveryReservation.getDestinationAddress();
+            destinationPostalCode = deliveryReservation.getDestinationPostalCode();
+            status = deliveryReservation.getDelivery().getStatus();
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class SimpleDeliveryDto {
+        private Long deliveryId;
+        private Long deliveryReservationId;
+        private Long storageReservationId;
+
+        private String arrivalTime;
+        private String status;
+
+        public SimpleDeliveryDto(Delivery delivery) {
+            deliveryId = delivery.getId();
+            deliveryReservationId = delivery.getDeliveryReservation().getId();
+            storageReservationId = delivery.getDeliveryReservation().getStorageReservation().getId();
+
+            arrivalTime = StringToDateTimeConverter.toDateString(delivery.getArrivalTime());
+            status = delivery.getStatus().name();
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class DeliveryDto {
+        private Long id;
+        private Double latitude;
+        private Double longitude;
+        private String arrivalTime;
+        private String status;
+
+        public DeliveryDto(Delivery delivery) {
+            id = delivery.getId();
+            latitude = delivery.getLatitude();
+            longitude = delivery.getLongitude();
+            arrivalTime = StringToDateTimeConverter.toDateString(delivery.getArrivalTime());
+            status = delivery.getStatus().name();
+        }
     }
 }
