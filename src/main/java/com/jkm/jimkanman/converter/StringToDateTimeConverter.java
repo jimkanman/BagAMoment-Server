73d08901753e2348a2e3ed77a219d449280f0e57
Jaehwan Hyun <jec0101@naver.com>
2024-11-29 15:56:45 +0900

    update: StringToDateTimeConverter 익셉션 catch하도록 변경

--- src/main/java/com/jkm/jimkanman/converter/StringToDateTimeConverter.java
+++ src/main/java/com/jkm/jimkanman/converter/StringToDateTimeConverter.java
@@ -7,20 +7,44 @@ public class StringToDateTimeConverter {
     public static DateTimeFormatter isoFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
     public static LocalDateTime toLocalDateTime(String dateString) {
-        return LocalDateTime.parse(dateString);
+        try {
+            return LocalDateTime.parse(dateString);
+        } catch (Exception e) {
+            System.out.println("StringToDateTimeConverter: WARNING - exception parsing " + dateString + ": " + e.getMessage());
+            return null;
+        }
     }
 
     public static LocalDateTime toLocalDateTime(String dateString, String format) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
-        return LocalDateTime.parse(dateString, formatter);
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+            return LocalDateTime.parse(dateString, formatter);
+        } catch (Exception e) {
+            System.out.println("StringToDateTimeConverter: WARNING - exception parsing " + dateString + " with format " + format + ": " + e.getMessage());
+            return null;
+        }
     }
 
     public static String toDateString(LocalDateTime dateTime) {
+        try {
+            return dateTime.format(isoFormatter);
+        } catch (Exception e) {
+            System.out.println("StringToDateTimeConverter: WARNING - exception parsing " + dateTime + ": exception = " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static String toRequiredDateString(LocalDateTime dateTime) {
         return dateTime.format(isoFormatter);
     }
 
     public static String toDateString(LocalDateTime dateTime, String format) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
-        return dateTime.format(formatter);
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+            return dateTime.format(formatter);
+        } catch (Exception e) {
+            System.out.println("StringToDateTimeConverter: WARNING - exception parsing " + dateTime + " with format " + format + ": " + e.getMessage());
+            return null;
+        }
     }
 }
